9ece257212d5ecb62d111d9e4287216daa6a9d49
Av0cat0 <75830242+Av0cat0@users.noreply.github.com>
2022-01-26 23:09:53 +0200

    Server

---
+++ Server/Controllers/TableDataController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UITable3.Shared;
+using System.Drawing;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UITable.Server.Controllers
+{
+    [Route("[controller]")]
+    public class TableDataController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<TableData> Get()
+        {
+            int amount = 10;
+            RandomFeatures Rf = new RandomFeatures();
+            List<TableData> data = new List<TableData>(); 
+            for (int i = 0; i < amount; i++)
+            {
+                var row = new TableData();
+                row.ActivityName = string.Concat(Rf.RandomFirstName(), Rf.RandomLastName());
+                row.ActivityDescription = Rf.RandomFirstName();
+                row.ActivityContext = Rf.RandomLastName();
+                row.Status = ((ActivityStatus)Rf.RandomNumber(Enum.GetNames(typeof(ActivityStatus)).Length)).ToString();
+                row.ServiceUserId = Guid.NewGuid();
+                row.CreatedDate = Rf.RandomDay();
+                row.DueDate = DateTime.Today.AddDays(Rf.RandomNumber(14));
+                row.Priority = ((PriorityStates)Rf.RandomNumber(Enum.GetNames(typeof(PriorityStates)).Length)).ToString();
+                row.Photo = (Rf.RandomNumber(8) + 1).ToString() + ".jpeg";
+                data.Add(row);
+            }
+            return data.ToArray();
+        }
+
+        // GET api/imagepath
+        /*[HttpGet("{imagePath}")]
+        public byte[] Get(string imagePath)
+        {
+            RandomFeatures Rf = new RandomFeatures();
+            return Image.FromFile((Rf.RandomNumber(8) + 1).ToString() + ".jpeg");
+        }*/
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
